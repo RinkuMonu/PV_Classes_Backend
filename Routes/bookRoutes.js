d0113a48b702555bdf687964a1706f9b84f7d1d6
nikichaturvedi <chaturvedinikita657@gmail.com>
2025-08-12 13:07:13 +0530

    Crud for book category,subcategory,books

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const bookController = require("../Controllers/bookController");
+const upload = require("../middleware/upload");
+
+// Create Book
+router.post(
+  "/",
+  upload("book").array("images", 5), // function call + folder name pass
+  bookController.createBook
+);
+
+// Get All Books
+router.get("/", bookController.getAllBooks);
+router.get("/:id", bookController.getBookById);
+router.get("/category/:categoryId", bookController.getBooksByCategoryId);
+router.put(
+  "/:id",
+  upload("book").array("images", 5),
+  bookController.updateBook
+);
+
+router.delete("/:id", bookController.deleteBook);
+
+module.exports = router;
