3bbf0cfe0e18433039737bdabda7787ae4c5e133
veer <veer123@gmail.com>
2025-09-04 16:34:28 +0530

    changes

--- Index.js
+++ Index.js
@@ -92,5 +92,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5006;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
